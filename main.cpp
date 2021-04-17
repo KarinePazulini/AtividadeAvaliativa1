84f6d010e392f29e069d2af442f608be07ff3954
karinePazu <>
2021-04-17 17:00:26 +0000

    Mudança de nome da variável para P, (enunciado)

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-string termo;
-//termo recebe a palavra desejada
+string P;
+//P vai guardar a palavra desejada
 
 int main() {
   cout << "Digite com no máximo 50 caracteres: \n";
-  cin >> termo;
+  cin >> P;
 
   int tecla; int qtde; int linha; int coluna;
   //tecla é a qual deve ser pressionada
@@ -16,18 +16,17 @@ int main() {
 
   char letras [4][8] = {{'a','d','g','j','m','p','t','w'}, {'b','e','h','k','n','q','u','x'}, {'c','f','i','l','o','r','v','y'}, {' ',' ',' ',' ',' ','s',' ','z'}};
 
-  for(int t = 0; t < size(termo); t++){
-    if(size(termo) > 50){
+  for(int t = 0; t < size(P); t++){
+    if(size(P) > 50){
       cout << "Termo inválido, tente novamente!" << endl;
       break;
     }else{
       for(int linha = 0; linha < 4; linha++){
         for(int coluna = 0; coluna < 8;coluna++){
-          if(termo[t] == letras[linha][coluna]){
+          if(P[t] == letras[linha][coluna]){
             tecla = coluna+2; qtde = linha+1;
             cout << "#" << tecla << "=" << qtde << endl;
           }
-
         }
       }
     }

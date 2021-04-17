0abf59272117bc01465b10f71304fcdb007ca5ac
karinePazu <>
2021-04-17 14:39:18 +0000

    Ajuste para colocar inválido termo maior que cinquenta

--- main.cpp
+++ main.cpp
@@ -14,11 +14,17 @@ int main() {
   char letras [4][8] = {{'a','d','g','j','m','p','t','w'}, {'b','e','h','k','n','q','u','x'}, {'c','f','i','l','o','r','v','y'}, {' ',' ',' ',' ',' ','s',' ','z'}};
 
   for(int t=0;t<size(termo);t++){
-    for(int linha=0; linha<4; linha++){
-      for(int coluna=0; coluna<8;coluna++){
-        if(termo[t] == letras[linha][coluna]){
-          tecla=coluna+2; qtde=linha+1;
-          cout << "#" << tecla << "=" << qtde << endl;
+    if(size(termo) > 50){
+      cout << "Inválida" << endl;
+      break;
+    }else{
+      for(int linha=0; linha<4; linha++){
+        for(int coluna=0; coluna<8;coluna++){
+          if(termo[t] == letras[linha][coluna]){
+            tecla=coluna+2; qtde=linha+1;
+            cout << "#" << tecla << "=" << qtde << endl;
+          }
+
         }
       }
     }
